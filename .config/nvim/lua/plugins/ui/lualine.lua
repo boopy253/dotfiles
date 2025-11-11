e7b89e68dbb4b4d09aa02ae0e86d907355ace220
boopy253 <boopy253@gmail.com>
2025-11-12 01:06:50 +0800

    feat(nvim-lualine): enhance LSP and formatter display in status line

--- .config/nvim/lua/plugins/ui/lualine.lua
+++ .config/nvim/lua/plugins/ui/lualine.lua
@@ -2,6 +2,46 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
+    -- Get the active lsp client
+    local function get_lsp_clients()
+      local clients = vim.lsp.get_active_clients({ bufnr = 0 })
+      if #clients == 0 then
+        return ""
+      end
+
+      local client_names = {}
+      local unique_names = {}
+
+      for _, client in ipairs(clients) do
+        if not unique_names[client.name] then
+          table.insert(client_names, client.name)
+          unique_names[client.name] = true
+        end
+      end
+
+      return " " .. table.concat(client_names, ", ")
+    end
+
+    -- Get conform configured formatter
+    local function get_formatter()
+      -- Make sure conform loaded
+      if not pcall(require, "conform") then
+        return ""
+      end
+
+      local formatters = require("conform").get_formatters(0) -- 0 for buffer
+      if #formatters == 0 then
+        return ""
+      end
+
+      local formatter_names = {}
+      for _, formatter in ipairs(formatters) do
+        table.insert(formatter_names, formatter.name)
+      end
+
+      return "󰄬 " .. table.concat(formatter_names, ", ")
+    end
+
     require("lualine").setup({
       options = {
         theme = "auto",
@@ -16,10 +56,6 @@ return {
             "diff",
             symbols = { added = " ", modified = " ", removed = " " },
           },
-          {
-            "diagnostics",
-            symbols = { error = " ", warn = " ", info = " " },
-          },
         },
         lualine_c = {
           {
@@ -28,6 +64,12 @@ return {
           },
         },
         lualine_x = {
+          { get_lsp_clients },
+          { get_formatter },
+          {
+            "diagnostics",
+            symbols = { error = " ", warn = " ", info = " " },
+          },
           "encoding",
           "filetype",
         },
